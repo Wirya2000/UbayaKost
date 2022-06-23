9904be2573338fdf3d732b22fcf4be3398b54e53
bryanimmanuelll29 <s160419015@student.ubaya.ac.id>
2022-06-23 17:54:37 +0700

    data_binding_init_2

--- app/src/main/java/id/ubaya/a160419033_ubayakost/view/Interfaces.kt
+++ app/src/main/java/id/ubaya/a160419033_ubayakost/view/Interfaces.kt
@@ -1,2 +1,23 @@
 package id.ubaya.a160419033_ubayakost.view
 
+import android.view.View
+import android.widget.CompoundButton
+
+interface FaqItemClickListener {
+    fun onItemClick(v: View)
+}
+
+// KOST LIST
+interface KostItemClickListener {
+    fun onItemClick(v:View)
+}
+
+// KOST DETAIL
+interface KostReviewClickListener {
+    fun onReviewClick(v:View)
+}
+
+// KOST DETAIL
+interface KostBookingClickListener {
+    fun onBookingClick(v:View)
+}
