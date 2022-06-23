ab7794d2056c46ece5932f7ab73be7feb19415d3
Wirya2000 <s160419036@student.ubaya.ac.id>
2022-06-23 17:43:44 +0700

    room model

--- app/src/main/java/id/ubaya/a160419033_ubayakost/model/Model.kt
+++ app/src/main/java/id/ubaya/a160419033_ubayakost/model/Model.kt
@@ -1,8 +1,140 @@
 package id.ubaya.a160419033_ubayakost.model
 
-data class Kost(var id: Int, var name: String, var gender: String, var region: String, var price: String, var photoUrl: String, var alamat: String, var luas: String, var fasilitas: String, var peraturan: String, var x: String, var y: String)
-data class Review(var id: Int, var name: String, var review: String)
-data class FAQ(var id: Int, var topic: String)
-data class Question(var id: Int, var question: String, var answer: String)
-data class Map(var latitude: Double, var longitude: Double, var place: String)
-data class Profile(var name: String?, var hometown: String?, var phoneNumber: String?)
+import androidx.room.*
+
+@Entity
+data class Kost(
+    @ColumnInfo(name="name")
+    var name: String,
+    @ColumnInfo(name="gender")
+    var gender: String,
+    @ColumnInfo(name="region")
+    var region: String,
+    @ColumnInfo(name="price")
+    var price: String,
+    @ColumnInfo(name="photoUrl")
+    var photoUrl: String,
+    @ColumnInfo(name="alamat")
+    var alamat: String,
+    @ColumnInfo(name="luas")
+    var luas: String,
+    @ColumnInfo(name="fasilitas")
+    var fasilitas: String,
+    @ColumnInfo(name="peraturan")
+    var peraturan: String,
+    @ColumnInfo(name="x")
+    var x: String,
+    @ColumnInfo(name="y")
+    var y: String
+) {
+    @PrimaryKey(autoGenerate = true)
+    var kostId: Int = 0
+}
+
+@Entity
+data class User(
+    @ColumnInfo(name="name")
+    var topic: String,
+    @ColumnInfo(name="hometown")
+    var hometown: String,
+    @ColumnInfo(name="phone_number")
+    var phone_number: String,
+) {
+    @PrimaryKey(autoGenerate = true)
+    var userId: Int = 0
+}
+
+@Entity(primaryKeys = ["kostId", "userId"])
+data class Review(
+    @ColumnInfo(name="kostId")
+    var kostId: Int,
+    @ColumnInfo(name="userId")
+    var userId: Int,
+    @ColumnInfo(name="name")
+    var name: String,
+    @ColumnInfo(name="review")
+    var review: String,
+    @ColumnInfo(name="photoUrl")
+    var photoUrl: String,
+    @ColumnInfo(name="alamat")
+    var alamat: String,
+)
+
+data class UserWithKost(
+    @Embedded val user: User,
+    @Relation(
+        parentColumn = "userId",
+        entityColumn = "kostId",
+        associateBy = Junction(Review::class)
+    )
+    val kosts: List<Kost>
+)
+
+data class KostWithUser(
+    @Embedded val kost: Kost,
+    @Relation(
+        parentColumn = "kostId",
+        entityColumn = "userId",
+        associateBy = Junction(Review::class)
+    )
+    val users: List<User>
+)
+
+@Entity
+data class FAQ(
+    @ColumnInfo(name="topic")
+    var topic: String,
+) {
+    @PrimaryKey(autoGenerate = true)
+    var faqId: Int = 0
+}
+
+@Entity
+data class Question(
+    @ColumnInfo(name="faqQuestionId")
+    var faqId: Int,
+    @ColumnInfo(name="question")
+    var question: Int,
+    @ColumnInfo(name="answer")
+    var answer: Int,
+) {
+    @PrimaryKey
+    var questionId: Int = 0
+}
+
+data class FaqWithPlaylist(
+    @Embedded val faq: FAQ,
+    @Relation(
+        parentColumn = "faqId",
+        entityColumn = "faqQuestionId"
+    )
+    val questions: List<Question>
+)
+
+@Entity(primaryKeys = ["kostId", "userId"])
+data class Booking(
+    @ColumnInfo(name="kostId")
+    var kostId: Int,
+    @ColumnInfo(name="userId")
+    var userId: Int
+)
+
+data class UserWithKostBooking(
+    @Embedded val user: User,
+    @Relation(
+        parentColumn = "userId",
+        entityColumn = "kostId",
+        associateBy = Junction(Booking::class)
+    )
+    val kosts: List<Kost>
+)
+
+data class KostWithUserBooking(
+    @Embedded val kost: Kost,
+    @Relation(
+        parentColumn = "kostId",
+        entityColumn = "userId",
+        associateBy = Junction(Booking::class)
+    )
+    val users: List<User>
+)
