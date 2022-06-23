ef21ae3a0c8b4d5d8d99ec4d22a2e157489faa9b
Joshua A.S <99380223+joshualbertus@users.noreply.github.com>
2022-06-23 15:08:14 +0700

    init notification helper

---
+++ app/src/main/java/id/ubaya/a160419033_ubayakost/util/NotificationHelper.kt
@@ -0,0 +1,48 @@
+package id.ubaya.a160419033_ubayakost.util
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import id.ubaya.a160419033_ubayakost.R
+import id.ubaya.a160419033_ubayakost.view.MainActivity
+
+class NotificationHelper(val context: Context) {
+    private val CHANNEL_ID = "kost_channel_id"
+    private val NOTIFICATION_ID = 1
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(CHANNEL_ID, CHANNEL_ID,
+                NotificationManager.IMPORTANCE_DEFAULT).apply {
+                description = "Kost channel description"
+            }
+
+            val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    fun createBookingNotification(title: String, message: String) {
+        createNotificationChannel()
+        val intent = Intent(context, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or
+                    Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent = PendingIntent.getActivity(context, 0, intent, 0)
+
+        val notification = NotificationCompat.Builder(context, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_baseline_check_circle_24)
+            .setContentTitle(title)
+            .setContentText(message)
+            .setContentIntent(pendingIntent)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .build()
+        NotificationManagerCompat.from(context).notify(NOTIFICATION_ID, notification)
+    }
+}
