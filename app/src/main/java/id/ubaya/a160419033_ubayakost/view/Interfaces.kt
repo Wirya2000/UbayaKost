5342d65b865386235be9f026fb22f4488beb6252
bryanimmanuelll29 <s160419015@student.ubaya.ac.id>
2022-06-29 13:56:01 +0700

    data_binding_init_4

--- app/src/main/java/id/ubaya/a160419033_ubayakost/view/Interfaces.kt
+++ app/src/main/java/id/ubaya/a160419033_ubayakost/view/Interfaces.kt
@@ -3,6 +3,7 @@ package id.ubaya.a160419033_ubayakost.view
 import android.view.View
 import id.ubaya.a160419033_ubayakost.model.Kost
 import android.widget.CompoundButton
+import id.ubaya.a160419033_ubayakost.model.User
 
 // FAQ LIST
 interface FaqItemClickListener {
@@ -22,4 +23,9 @@ interface KostReviewClickListener {
 // KOST DETAIL
 interface KostBookingClickListener {
     fun onBookingClick(v:View, obj:Kost)
+}
+
+// PROFILE
+interface ProfileUpdateCLickListener {
+    fun onUpdateClick(v:View, obj: User)
 }
--- app/src/main/java/id/ubaya/a160419033_ubayakost/view/ProfileFragment.kt
+++ app/src/main/java/id/ubaya/a160419033_ubayakost/view/ProfileFragment.kt
@@ -8,61 +8,50 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.content.edit
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.Navigation
 import id.ubaya.a160419033_ubayakost.R
+import id.ubaya.a160419033_ubayakost.databinding.FragmentProfileBinding
+import id.ubaya.a160419033_ubayakost.model.User
 import id.ubaya.a160419033_ubayakost.viewmodel.ProfileViewModel
 import kotlinx.android.synthetic.main.fragment_profile.*
 
-class ProfileFragment : Fragment() {
-    companion object {
-        var SHARED_PROFILE_NAME = "SHARED_PROFILE_NAME"
-        var SHARED_PROFILE_HOMETOWN = "SHARED_PROFILE_HOMETOWN"
-        var SHARED_PROFILE_PHONE = "SHARED_PROFILE_PHONE"
-    }
+class ProfileFragment : Fragment(), ProfileUpdateCLickListener {
     private lateinit var viewModel: ProfileViewModel
+    private lateinit var dataBinding: FragmentProfileBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        dataBinding = DataBindingUtil.inflate<FragmentProfileBinding>(inflater, R.layout.fragment_profile,
+            container, false)
+        return dataBinding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        dataBinding.listener = this
+
         viewModel = ViewModelProvider(this).get(ProfileViewModel::class.java)
-
-        val sharedPreferences: SharedPreferences = requireContext().getSharedPreferences("sharedPref", Context.MODE_PRIVATE)
-        var name = sharedPreferences.getString(SHARED_PROFILE_NAME, "")
-        var hometown = sharedPreferences.getString(SHARED_PROFILE_HOMETOWN, "")
-        var phone = sharedPreferences.getString(SHARED_PROFILE_PHONE, "")
-
-        viewModel.fetch(name, hometown, phone)
 
         observeViewModel()
     }
 
     private fun observeViewModel() {
         viewModel.profileLiveData.observe(viewLifecycleOwner) {
-            val profile = it
-                profile?.let {
-                    editName.setText(it.name)
-                    editHomeTown.setText(it.hometown)
-                    editPhone.setText(it.phoneNumber)
-                }
-            buttonUpdate.setOnClickListener{
-                val sharedPreferences: SharedPreferences = requireContext().getSharedPreferences("sharedPref", Context.MODE_PRIVATE)
-                sharedPreferences.edit {
-                    putString(SHARED_PROFILE_NAME, editName.text?.toString())
-                    putString(SHARED_PROFILE_HOMETOWN, editHomeTown.text?.toString())
-                    putString(SHARED_PROFILE_PHONE, editPhone.text?.toString())
-                }
-                val builder = AlertDialog.Builder(context)
-                builder.setMessage("Your profile has been updated")
-                builder.setPositiveButton("OK", null)
-                builder.create().show()
-            }
+            dataBinding.user = it
         }
+    }
+
+    override fun onUpdateClick(v: View, obj: User) {
+        viewModel.update(obj.name, obj.hometown, obj.phone_number)
+        Toast.makeText(v.context, "Profile updated", Toast.LENGTH_SHORT).show()
+        Navigation.findNavController(v).popBackStack()
     }
 }
